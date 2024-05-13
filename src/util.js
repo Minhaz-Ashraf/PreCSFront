8e5241ec08eaad5b16ba266de1345e42dfe5a384
Minhaz Ashraf <minhazashraf590@gmail.com>
2024-05-14 02:23:22 +0530

    dfhjbkdfbh

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiurl = axios.create({
-  baseURL: "http://cspreadmin.gauravdesign.com/",
+  baseURL: "https://cspreadmin.gauravdesign.com",
   // http://localhost:8000
   // You can add other default configurations here if needed
 });
